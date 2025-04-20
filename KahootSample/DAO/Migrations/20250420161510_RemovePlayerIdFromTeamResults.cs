bae43b26768b8ba70759c6da91200879c85963ce
MOI <hungnvse150584@fpt.edu.vn>
2025-04-20 23:31:31 +0700

    Fix Migrations

--- KahootSample/DAO/Migrations/20250420161510_RemovePlayerIdFromTeamResults.cs
+++ KahootSample/DAO/Migrations/20250420161510_RemovePlayerIdFromTeamResults.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace DAO.Migrations
 {
     /// <inheritdoc />
-    public partial class UpdateSchema : Migration
+    public partial class RemovePlayerIdFromTeamResults : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
