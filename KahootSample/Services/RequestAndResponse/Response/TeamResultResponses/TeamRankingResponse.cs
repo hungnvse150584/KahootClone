dea041394f09a23a6ea75894b7265c2aeedcfd44
duyptse182301 <duyptse182301@fpt.edu.vn>
2025-05-03 13:42:02 +0700

    Add team name and ranking in leaderboard

--- KahootSample/Services/RequestAndResponse/Response/TeamResultResponses/TeamRankingResponse.cs
+++ KahootSample/Services/RequestAndResponse/Response/TeamResultResponses/TeamRankingResponse.cs
@@ -10,7 +10,9 @@ namespace Services.RequestAndResponse.Response.TeamResultResponses
     public class TeamRankingResponse
     {
         public int TeamId { get; set; }
+        public string TeamName { get; set; }
         public int TotalScore { get; set; }
+        public int Rank { get; set; }
     }
 
 }
